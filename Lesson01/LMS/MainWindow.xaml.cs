1152e3bbd9d8255ccce50427a300abef78c9e8b4
j_tohirun <ibnalisher1@gmail.com>
2024-04-20 16:47:01 +0500

    code review fixes

--- Lesson01/LMS/MainWindow.xaml.cs
+++ Lesson01/LMS/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace LMS
         {
             InitializeComponent();
 
-            int a = "hello";
+            string a = "hello";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
