22c9eadadce0b221fa263126acf40ed80aef80f4
j_tohirun <ibnalisher1@gmail.com>
2024-04-23 19:52:00 +0500

    employyeeManagemntView was added for view

---
+++ Lesson02/LMS/Views/AddEmployeeDialog.xaml.cs
@@ -0,0 +1,73 @@
+﻿using LMS.Data;
+using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LMS.Views
+{
+    /// <summary>
+    /// Interaction logic for AddEmployeeDialog.xaml
+    /// </summary>
+    public partial class AddEmployeeDialog : Window
+    {
+        private readonly EmployeeManagement _databaseManager;
+        private readonly bool isEditingMode;
+
+        public AddEmployeeDialog()
+        {
+            InitializeComponent();
+
+            hiredateInput.SelectedDate = DateTime.Now;
+            _databaseManager = new EmployeeManagement();
+        }
+
+        public AddEmployeeDialog(Employee employee)
+            : this()
+        {
+            isEditingMode = true;
+            empnoInput.IsEnabled = false;
+
+            empnoInput.Text = employee.Empno.ToString();
+            enameInput.Text = employee.Ename.ToString();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var empno = decimal.Parse(empnoInput.Text);
+            var ename = enameInput.Text;
+            var job = jobInput.Text;
+            decimal? mgr = string.IsNullOrEmpty(mgrInput.Text) ? null : decimal.Parse(mgrInput.Text);
+            var hiredate = hiredateInput.SelectedDate ?? DateTime.Now;
+            var sal = decimal.Parse(salInput.Text);
+            decimal? comm = string.IsNullOrEmpty(commInput.Text) ? null : decimal.Parse(commInput.Text);
+            var deptno = decimal.Parse(deptnoInput.Text);
+
+            var newEmployee = new Employee(empno, ename, job, mgr, hiredate, sal, comm, deptno);
+            
+            bool isSuccess;
+
+            if (isEditingMode)
+            {
+                isSuccess = _databaseManager.UpdateEmployee(employee);
+            }
+            else
+            {
+                isSuccess = _databaseManager.CreateEmployee(newEmployee);
+            }
+
+            if (isSuccess)
+                Close();
+        }
+    }
+}
---
+++ Lesson02/LMS/Views/EmployeesWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using LMS.Data;
+using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LMS.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeesWindow.xaml
+    /// </summary>
+    public partial class EmployeesWindow : Window
+    {
+        private readonly EmployeeManagement _databaseManager;
+
+        public EmployeesWindow()
+        {
+            InitializeComponent();
+
+            _databaseManager = new EmployeeManagement();
+
+            var employees = _databaseManager.GetEmployees();
+
+            EmployeesDataGrid.ItemsSource = employees;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new AddEmployeeDialog();
+
+            dialog.ShowDialog();
+
+            var employees = _databaseManager.GetEmployees();
+
+            EmployeesDataGrid.ItemsSource = null;
+            EmployeesDataGrid.ItemsSource = employees;
+        }
+
+        private void EmployeesDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var employee = EmployeesDataGrid.SelectedItem as Employee;
+
+            if (employee != null)
+            {
+                var window = new AddEmployeeDialog(employee);
+                window.ShowDialog();
+            }
+        }
+    }
+}
