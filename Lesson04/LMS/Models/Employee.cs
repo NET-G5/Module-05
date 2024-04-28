c6747f40aa1c7e84a9e5694623dc5e9d5a254ccc
j_tohirun <ibnalisher1@gmail.com>
2024-04-28 16:59:50 +0500

    entity models were added

---
+++ Lesson04/LMS/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LMS.Models;
+
+public class Employee
+{
+    public decimal Empno { get; set; }
+    public string Ename { get; set; }
+    public string Job { get; set; }
+    public decimal? Mgr { get; set; }
+    public DateTime Hiredate { get; set; }
+    public decimal Salary { get; set; }
+    public decimal? Comm { get; set; }
+    public decimal Deptno { get; set; }
+
+    public Employee() { }
+    
+    public Employee(decimal empno,  string ename, string job,
+        decimal? mgr,  DateTime hiredate,  decimal salary, 
+        decimal? comm, decimal deptno)
+    {
+        Empno = empno;
+        Ename = ename;
+        Job = job;
+        Mgr = mgr;
+        Hiredate = hiredate;
+        Salary = salary;
+        Comm = comm;
+        Deptno = deptno;
+    }
+
+    public override string ToString()
+    {
+        return $"{Ename} ({Empno})";
+    }
+}
---
+++ Lesson04/LMS/Models/Salgrade.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Models;
+
+class Salgrade
+{
+    public int Grade { get; set; }
+    public decimal Losal { get; set; }
+    public decimal Hisal { get; set; }
+}
