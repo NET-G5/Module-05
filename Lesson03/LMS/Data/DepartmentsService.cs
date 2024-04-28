0fe7d50f27f4f652b5d61ae5140ef77e44c7e93d
j_tohirun <ibnalisher1@gmail.com>
2024-04-28 16:56:51 +0500

    DepartmentService and employeeManagement were added

---
+++ Lesson03/LMS/Data/DepartmentsService.cs
@@ -0,0 +1,50 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace LMS.Data;
+
+internal class DepartmentsService
+{
+    public List<Department> GetDepartments()
+    {
+        SqlConnection sqlConnection = new SqlConnection("Data Source=TOHIRJON-LAPTOP;Initial Catalog=employee_management;Integrated Security=True");
+        List<Department> departments = new List<Department>();
+
+        try
+        {
+            sqlConnection.Open();
+
+            var command = sqlConnection.CreateCommand();
+            command.CommandText = "SELECT * FROM Dept\n" +
+                "ORDER BY Dname, Loc";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var deptno = reader.GetDecimal(0);
+                var dname = reader.GetString(1);
+                var loc = reader.GetString(2);
+
+                var department = new Department(deptno, dname, loc);
+                departments.Add(department);
+            }
+        }
+        catch(Exception ex)
+        {
+            MessageBox.Show($"Error fetching departments. Please, try again later.\nDetails: {ex.Message}",
+                "Database error.",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        finally
+        {
+            sqlConnection.Close();
+        }
+
+        return departments;
+    }
+}
